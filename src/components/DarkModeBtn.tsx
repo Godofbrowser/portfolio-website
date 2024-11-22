8da8d475c8054a68f1da03a1695e94d36d3da3b7
Emeke Ajeh <ajemeke@gmail.com>
2024-11-22 14:24:48 +0100

    feat: improved theming and transition

--- src/components/DarkModeBtn.tsx
+++ src/components/DarkModeBtn.tsx
@@ -1,9 +1,10 @@
 "use client"
 
 import { ClientComponent } from "@/containers/ClientComponent";
-import { useTheme } from "@/hooks/useTheme";
+import { useTheme } from "next-themes";
 import { cn } from "@/lib/utils";
 import { AppIcon } from "./ui/AppIcon";
+import { Button } from "./ui/button";
 
 export interface DarkModeBtnProps {
     className?: string;
@@ -13,18 +14,23 @@ const DarkModeBtn = ({ className }: DarkModeBtnProps) => {
     const { theme, setTheme } = useTheme()
     return (
       <ClientComponent>
-        <button
-          className={cn(className || "", "hover:text-accent")}
+        <Button
+          className={cn(className || "", "[&_svg]:size-6")}
           onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
           aria-label="Theme mode toggle"
+          size="icon"
+          variant="ghost"
         >
-          {
             <AppIcon
-              className="size-7"
-              name={theme === "dark" ? "sun" : "moon"}
+                className="scale-100 rotate-0 dark:scale-0 dark:-rotate-90"
+                name={"moon"}
             />
-          }
-        </button>
+            <AppIcon
+                className="absolute scale-0 rotate-90 dark:rotate-0 dark:scale-100"
+                name={"sun"}
+            />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
       </ClientComponent>
     );
 }
--- src/containers/ThemeProvider.tsx
+++ src/containers/ThemeProvider.tsx
@@ -1,56 +1,9 @@
-"use client"
+"use client";
 
-import { ThemeProviderContext } from "@/context/theme-context"
-import { Theme } from "@/interfaces/theme"
-import { useEffect, useState } from "react"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
-
-
-type ThemeProviderProps = {
-    children: React.ReactNode
-    defaultTheme?: Theme
-    storageKey?: string
-  }
-
-export function ThemeProvider({
-    children,
-    defaultTheme = "system",
-    storageKey = "app-theme",
-    ...props
-  }: ThemeProviderProps) {
-    const [theme, setTheme] = useState<Theme>(
-      () => (typeof window === 'undefined' ? defaultTheme : localStorage.getItem(storageKey) as Theme) || defaultTheme
-    )
-  
-    useEffect(() => {
-      const root = window.document.documentElement
-  
-      root.classList.remove("light", "dark")
-  
-      if (theme === "system") {
-        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-          .matches
-          ? "dark"
-          : "light"
-  
-        root.classList.add(systemTheme)
-        return
-      }
-  
-      root.classList.add(theme)
-    }, [theme])
-  
-    const value = {
-      theme,
-      setTheme: (theme: Theme) => {
-        localStorage.setItem(storageKey, theme)
-        setTheme(theme)
-      },
-    }
-  
-    return (
-      <ThemeProviderContext.Provider {...props} value={value}>
-        {children}
-      </ThemeProviderContext.Provider>
-    )
-  }
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
